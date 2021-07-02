b743a53885900b488856dd600a556a0f1e287f74
Yoann Anthony <yoann.anthony@protonmail.ch>
2021-07-02 12:23:33 +0200

    Scoping in Practice lesson

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -4,9 +4,21 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age} years old born in ${birthYear}`;
+    const output = `${firstName}, You are ${age} years old born in ${birthYear}`;
     console.log(output);
   }
+
+  if (birthYear >= 1981 && birthYear <= 1996) {
+    var millenial = true;
+    const str = `oh and you're a Millenial ${firstName}`;
+    console.log(str);
+    function add(a, b) {
+      return a + b;
+    }
+  }
+  // console.log(str);
+  console.log(millenial);
+  // console.log(add(3, 2));
   printAge();
   return age;
 }
